02b48f73fed529169f769a9a420639856b5b2054
Lapis Elazul <lapiselazul@proton.me>
2025-05-19 21:44:07 +0300

    prog: modal

--- src/components/full-image.page.tsx
+++ src/components/full-image.page.tsx
@@ -2,8 +2,16 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
-  
-  
 
-  return <img src={image.url} className="h-auto w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex shrink justify-center items-center">
+      <img src={image.url} className="h-auto object-contain flex shrink" />
+      </div>
+
+      <div className="w-48 flex flex-col shrink-0 border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
