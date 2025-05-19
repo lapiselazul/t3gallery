26fc349f92dc16f38c0bdbaa591a3b5013856694
Lapis Elazul <lapiselazul@proton.me>
2025-05-19 13:57:14 +0300

    chore: enable Sentry

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://91f99cafc4ec2f8555c4bea99458f1bf@o4509349340643328.ingest.de.sentry.io/4509349347459152",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
