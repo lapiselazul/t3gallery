16373c5eafb19a6863c0fdcecc4a34f42f3e4c0c
Lapis Elazul <lapiselazul@proton.me>
2025-05-19 21:28:23 +0300

    fix: responsive images

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -12,22 +12,21 @@ export default async function Images() {
   }
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
+    <div className="flex flex-wrap justify-center gap-8 p-8">
       {images.length === 0 ? (
         <p>Please upload an image.</p>
       ) : (
         images.map((image) => (
-          <div
-            key={image.id}
-            className="flex w-48 h-48 flex-col items-center"
-          >
+          <div key={image.id} className="block h-48 lg:h-64 w-48 lg:w-64 bg-red-300 text-center">
             <Link href={`/img/${image.id}`}>
               <Image
                 src={image.url}
                 width={192}
                 height={192}
                 alt={image.name}
+                className="object-cover h-48 lg:h-64 w-full"
               />
+              <div>{image.name}</div>
             </Link>
           </div>
         ))
