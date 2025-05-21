c086d2d5669512495ada20da0e769f8d41ee0f45
Lapis Elazul <lapiselazul@proton.me>
2025-05-21 21:47:19 +0300

    feat: allow delete only for image owners

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,12 +1,10 @@
-import { auth, clerkClient } from "@clerk/nextjs/server";
-import Trash from "~/app/_components/trash";
-import { deleteImage, getImage } from "~/server/queries";
+import { clerkClient } from "@clerk/nextjs/server";
+import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
   const client = await clerkClient();
-  const imageUser = await client.users.getUser(image.userId);
-  const loggedUser = await auth();
+  const user = await client.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-full">
@@ -25,31 +23,12 @@ export default async function FullPageImageView({ id }: { id: number }) {
 
         <div className="flex flex-col p-4">
           <span>Uploaded By:</span>
-          <span>{imageUser.fullName}</span>
+          <span>{user.fullName}</span>
         </div>
         <div className="flex flex-col p-4">
           <span>Created On:</span>
           <span>{new Date(image.createdAt).toLocaleDateString()}</span>
         </div>
-
-        {loggedUser?.userId === image?.userId && (
-          <div className="p-4">
-            <form
-              action={async () => {
-                "use server";
-
-                await deleteImage(id);
-              }}
-            >
-              <button
-                type="submit"
-                className="me-2 mb-2 flex items-center gap-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300 focus:outline-none dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-              >
-                <Trash /> Delete Image
-              </button>
-            </form>
-          </div>
-        )}
       </div>
     </div>
   );
