4e85595a453ce01dc6d87b7bdb2d6ca13548f183
Lapis Elazul <lapiselazul@proton.me>
2025-05-21 21:24:11 +0300

    feat: add delete server action

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,29 +1,55 @@
-import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import Trash from "~/app/_components/trash";
+import { deleteImage, getImage } from "~/server/queries";
 
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
   const client = await clerkClient();
-  const user = await client.users.getUser(image.userId);
+  const imageUser = await client.users.getUser(image.userId);
+  const loggedUser = await auth();
 
   return (
     <div className="flex h-full w-full">
-      <div className="flex flex-1 items-center justify-center min-w-0">
-        <img src={image.url} className="object-contain max-w-full max-h-full" alt={image.name} />
+      <div className="flex min-w-0 flex-1 items-center justify-center">
+        <img
+          src={image.url}
+          className="max-h-full max-w-full object-contain"
+          alt={image.name}
+        />
       </div>
 
-      <div className="flex flex-col w-96 shrink-0 border-l">
-        <div className="border-b text-center text-lg p-2">{image.name.toUpperCase()}</div>
+      <div className="flex w-96 shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">
+          {image.name.toUpperCase()}
+        </div>
 
         <div className="flex flex-col p-4">
           <span>Uploaded By:</span>
-          <span>{user.fullName}</span>
+          <span>{imageUser.fullName}</span>
         </div>
         <div className="flex flex-col p-4">
           <span>Created On:</span>
           <span>{new Date(image.createdAt).toLocaleDateString()}</span>
         </div>
-        
+
+        {loggedUser?.userId === image?.userId && (
+          <div className="p-4">
+            <form
+              action={async () => {
+                "use server";
+
+                await deleteImage(id);
+              }}
+            >
+              <button
+                type="submit"
+                className="me-2 mb-2 flex items-center gap-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300 focus:outline-none dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              >
+                <Trash /> Delete Image
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -2,6 +2,9 @@ import "server-only";
 
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
+import { images } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export async function getUserImages(userId: string | null) {
   let images = [];
@@ -25,4 +28,16 @@ export async function getImage(id: number) {
 
   if (!image) throw new Error("Image not found");
   return image;
+}
+
+export async function deleteImage(id: number) {
+  const user = await auth();
+  if (!user.userId) {
+    throw new Error("Unauthorized");
+  }
+
+  await db.delete(images)
+    .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+    redirect("/");
 }
